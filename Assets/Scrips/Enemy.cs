91b44a3c155ffb3c042e2a7113179c0b4269a0ce
oriana143 <oriana1143@gmail.com>
2022-03-22 15:56:51 +0100

    commit

--- Assets/Scrips/Enemy.cs
+++ Assets/Scrips/Enemy.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    void OnParticleCollision(GameObject other) 
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 1;
+    [SerializeField] int hitPoints = 4;
+
+    ScoreBoard scoreBoard;
+
+    void Start() 
     {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+    
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints --;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); // identity = no rotation necessary. transform position = where we are at the moment
+        vfx.transform.parent = parent;
         Destroy(GameObject.FindWithTag("Enemy"));
     }
+
 }
